1d7d8d66e4ae8d4c13224c8649906f29f91687fd
Taylor Gonzales <hello@taylorgonz.com>
2021-03-13 21:10:44 -0500

    building out init prompt and switch statement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { createConnection } = require('mysql');
 const inquirer = require('inquirer');
 
-const client = createConnection ({
+const client = createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
@@ -11,21 +11,46 @@ const client = createConnection ({
 
 const init = () => {
     inquirer
-    .prompt([
-        {
-            type:'list',
-            name:'choices',
-            message: 'What would you like to do?',
-            choices: ['View all employees', 'Add employee', 'Add department', 'Update employee manager', 'Update employee role' ]
-        }
-    ])
-    
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: ['View all employees', 'Add employee', 'Add department', 'Update employee manager', 'Update employee role']
+            }
+        ]).then((answers) => {
+            switch (answers.action) {
+                case 'View all employees':
+                    
+                    break;
+
+                case 'Add employees':
+
+                    break;
+                case 'Add department':
+
+                    break;
+                case 'Update employeed manager':
+
+                    break;
+                case 'Update employee role':
+
+                    break;
+            }
+
+
+        })
+
     client.end();
 }
 
-client.connect ((err) => {
-    if(err) throw err;
+const viewEmployees = () => {
+    const query = 'SELECT * FROM '
+}
+
+client.connect((err) => {
+    if (err) throw err;
     console.log(`connected as id: ${client.threadId}`);
     init();
-    
+
 })
