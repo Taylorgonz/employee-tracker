999591969292fe428b7f187ee99c9922e3b2af82
Taylor Gonzales <hello@taylorgonz.com>
2021-03-13 21:27:12 -0500

    auto increment for id's

--- employee_trackerDB.sql
+++ employee_trackerDB.sql
@@ -2,24 +2,24 @@ DROP DATABASE IF EXISTS employee_trackerdb;
 CREATE database employee_trackerdb;
 USE employee_trackerdb;
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     PRIMARY KEY (id)
 );
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
+    
 );
 
 SELECT * FROM department;
--- index.js
+++ index.js
@@ -16,21 +16,27 @@ const init = () => {
                 type: 'list',
                 name: 'action',
                 message: 'What would you like to do?',
-                choices: ['View all employees', 'Add employee', 'Add department', 'Update employee manager', 'Update employee role']
+                choices: ['View all employees', 'View all departments', 'View all roles', 'Add employee', 'Add department', 'Add role', 'Update employee role']
             }
         ]).then((answers) => {
             switch (answers.action) {
                 case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'View all departments':
+                    
+                    break;
+                case 'View all roles':
                     
                     break;
 
                 case 'Add employees':
-
+                    
                     break;
                 case 'Add department':
 
                     break;
-                case 'Update employeed manager':
+                case 'Add role':
 
                     break;
                 case 'Update employee role':
@@ -45,7 +51,16 @@ const init = () => {
 }
 
 const viewEmployees = () => {
-    const query = 'SELECT * FROM '
+    const query = 'SELECT * FROM employee',
+    (err,res) => {
+        if(err) throw  err;
+        res.forEach(( {id, first_name, last_name}) => {
+            console.table(id, first_name,last_name);
+        })
+        console.log('---------------------------------');
+    }
+    init();
+
 }
 
 client.connect((err) => {
